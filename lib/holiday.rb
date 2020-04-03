b83c72536c5f47c7b200fc9cb3df0cd7d253372e
Beverly Appleton <bfcain19@gmail.com>
2020-04-03 09:12:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_winter_holiday_supplies(holiday_hash)
       supplies.concat(supplies)
     end
     supplies
-  
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
